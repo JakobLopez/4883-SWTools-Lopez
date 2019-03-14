ba70dec49baa28618ff5937d7f3490160a87bb92
JakobLopez <thejakoblopez@gmail.com>
2019-03-14 16:34:22 -0500

    resizes images

--- Assignments/A07/match.py
+++ Assignments/A07/match.py
@@ -1,4 +1,4 @@
-from skimage.measure import compare_ssim as ssim
+from skimage.measure import compare_ssim 
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
@@ -9,12 +9,31 @@ def mse(imageA, imageB):
 	# the 'Mean Squared Error' between the two images is the
 	# sum of the squared difference between the two images;
 	# NOTE: the two images must have the same dimension
+
+	#esized_image = cv2.resize(image, (100, 50))
+	imageA,imageB = resize(imageA,imageB)
 	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
 	err /= float(imageA.shape[0] * imageA.shape[1])
 	
 	# return the MSE, the lower the error, the more "similar"
 	# the two images are
 	return err
+
+def ssim(imageA, imageB):
+	imageA,imageB = resize(imageA,imageB)
+	
+	s = compare_ssim(imageA,imageB)
+
+	return s
+
+def resize(a,b):
+	ah, aw= a.shape
+	bh, bw = b.shape
+	if (ah*aw) < (bh*bw):
+		b = cv2.resize(b, (aw,ah))
+	elif (ah*aw) > (bh*bw):
+		a = cv2.resize(a, (bw,bh))
+	return a,b
 
 def get_arguments():
 	# This assumes arguments are like: key1=val1 key2=val2 (with NO spaces between key equal val!)
@@ -54,15 +73,18 @@ for dirname, dirnames, filenames in os.walk(args['folder']):
 		
 		#Get mse of original and current image
 		m = mse(original,comparison)
+
 		#Get ssim of original and current image 
 		s = ssim(original,comparison)
+		#If compared image is not the original
+		if(s != 1 and m != 0):
+			#Add results to dictionary
+			comparisons[path] = {}
+			comparisons[path]['image'] = comparison
+			comparisons[path]['path'] = path
+			comparisons[path]['mse'] = m
+			comparisons[path]['ssim'] = s
 
-		#Add results to dictionary
-		comparisons[path] = {}
-		comparisons[path]['image'] = comparison
-		comparisons[path]['path'] = path
-		comparisons[path]['mse'] = m
-		comparisons[path]['ssim'] = s
 #Get path to images closest to original according to mse and ssim
 lowest_mse = min(comparisons, key=lambda x:comparisons[x]['mse'])
 highest_ssim = max(comparisons, key=lambda x:comparisons[x]['ssim'])
