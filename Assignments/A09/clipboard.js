489f72bf3c2bd006cec00083b87256c562d0cf5a
JakobLopez <thejakoblopez@gmail.com>
2019-04-28 17:06:24 -0500

    multiple text selection and copy

--- Assignments/A09/clipboard.js
+++ Assignments/A09/clipboard.js
@@ -1,74 +1,42 @@
-function getTextSelection() {
+function hightlightTextSelection() {
 
     //W3C or Mozilla Range Object
     //Supported on Chrome, Edge, Safari, IE 9 +
     //Table to supported browsers : https://developer.mozilla.org/en-US/docs/Web/API/Range
     if (window.getSelection) {
-
-        //Get selection from window
-        var selectionObj = window.getSelection();
-        var sel = selectionObj.toString();
-        /*
-
-        //If browser supports getRangeAt
-        if (selectionObj.getRangeAt)
-            // Get range starting from index 0
-            var range = selectionObj.getRangeAt(0);
-        //For Safari, doesn't support getRangeAt    
-        else {
-            //Create new range object
-            var range = document.createRange();
-
-            //Set start and end of range
-            range.setStart(selectionObjel.anchorNode, selectionObj.anchorOffset);
-            range.setEnd(selectionObj.focusNode, selectionObj.focusOffset);
+        var range, sel = window.getSelection();
+        if (sel.rangeCount && sel.getRangeAt) {
+            range = sel.getRangeAt(0);
         }
 
-        //Get text from selected range
-        var text = range.cloneContents();
-        var div = document.createElement('div');
-        div.appendChild(text);*/
+        document.designMode = "on";
 
-        /*
-        if (!document.execCommand("copy", false)) {
-            makeEditableAndHighlight('yellow');
-        }*/
+        if (range) {
+            sel.removeAllRanges();
+            sel.addRange(range);
+        }
 
-        return sel;
-    }
-    //For browsers that use Microsoft Text Range Objects
-    else if (document.selection) {
-        var sel = document.selection.createRange();
+        // Use HiliteColor since some browsers apply BackColor to the whole block
+        if (!document.execCommand("HiliteColor", false, '#3297FD')) {
+            document.execCommand("BackColor", false, '#3297FD');
+        }
 
-        var selectedText = userSelection;
+        //Change text color to white
+        document.execCommand('foreColor', false, 'white');
 
-        if (userSelection.text)
-            selectedText = userSelection.text;
-        return selectedText;
-        //range.execCommand("BackColor", false, 'yellow');
+        document.designMode = "off";
     }
 
 }
 
-/**
- * Gets text currently in clipboard
- * @param none
- * @returns text in clipboard
- */
-async function getClipboardText() {
-    try {
-        //Gets read permission
-        var readResult = await navigator.permissions.query({
-            name: "clipboard-read"
-        });
-
-        //If permission allowed
-        if (readResult.state == "granted" || readResult.state == "prompt")
-            //Return text in clipboard
-            return await navigator.clipboard.readText();
-    } catch (e) {
-        alert('An error occured reading from the clipboard');
+function getTextSelection() {
+    var fontList = document.getElementsByTagName('font');
+    var fontContent = "";
+    for (let font of fontList) {
+        if (font.getAttribute('color') == '#ffffff')
+            fontContent = fontContent + font.innerText + " ";
     }
+    return fontContent;
 }
 
 /**
@@ -90,26 +58,9 @@ async function writeToClipboard(newClip) {
     }
 }
 
-function makeEditableAndHighlight(colour) {
-    var range, sel = window.getSelection();
-    if (sel.rangeCount && sel.getRangeAt) {
-        range = sel.getRangeAt(0);
-    }
-    document.designMode = "on";
-    if (range) {
-        sel.removeAllRanges();
-        sel.addRange(range);
-    }
-    // Use HiliteColor since some browsers apply BackColor to the whole block
-    if (!document.execCommand("HiliteColor", false, colour)) {
-        document.execCommand("BackColor", false, colour);
-    }
-    document.designMode = "off";
-}
 
+document.addEventListener('mousedown', hightlightTextSelection);
 
-
-document.addEventListener('mousedown', getTextSelection);
 /**
  * Listens for a request from the button in the browser.
  * When it sees the getTextSelection request, it returns the selection HTML, as well as the URL and title of the tab.
@@ -121,11 +72,9 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         //Get content from selection
         var selection = getTextSelection();
 
-        //Get text already in the clipboard
-        getClipboardText().then(text => {
-            //Append currently selected text to what is already in clipboard
-            writeToClipboard(text + selection);
 
+        //Append currently selected text to what is already in clipboard
+        writeToClipboard(selection).then(() => {
             //Send response back to background.ts
             sendResponse({
                 body: selection,
@@ -133,6 +82,9 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                 subject: document.title
             });
         });
+
+
+
 
     } else
         //Send back a blank message to background.ts
