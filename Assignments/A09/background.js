88bad2bf5b3a797fa6c22d01877afdb7b8e109bf
JakobLopez <thejakoblopez@gmail.com>
2019-05-04 12:34:15 -0500

    added keyboard shortcut to copy text

--- Assignments/A09/background.js
+++ Assignments/A09/background.js
@@ -2,7 +2,7 @@
 //addListener => 
 //              1. function sends message to current tab
 //              2. callback function after tab sends response
-chrome.browserAction.onClicked.addListener(function (tab) {
+/*chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.sendMessage(tab.id, {
         method: "getTextSelection"
     }, function (response) {
@@ -14,4 +14,31 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 
         alert(url + '\n' + subject + '\n' + body);
     });
+});*/
+
+//Listen for a keyboard shortcut
+chrome.commands.onCommand.addListener(function (command) {
+    //If Ctrl+Shift+5
+    if (command == 'copy-selected-text') {
+        //Get current tab
+        chrome.tabs.query({
+            currentWindow: true
+        }, function (tab) {
+
+            //Send message to copy selected text from window
+            chrome.tabs.sendMessage(tab[0].id, {
+                method: "getTextSelection"
+            }, 
+            //Callback function to alert selected text and other information about window
+            function (response) {
+
+                //Get response from content.js
+                var url = response.url;
+                var subject = response.subject;
+                var body = response.body;
+
+                alert(url + '\n' + subject + '\n' + body);
+            });
+        });
+    }
 });
